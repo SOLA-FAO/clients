44a613184e105c5a865308cd7b34f0bdb34d1968
Andrew McDowell <armcdowell@hotmail.com>
2012-04-01 03:45:56 +0200

    Customization Release

--- swing/desktop/src/main/java/org/sola/clients/swing/desktop/AboutForm.java
+++ swing/desktop/src/main/java/org/sola/clients/swing/desktop/AboutForm.java
@@ -122,7 +122,7 @@ public class AboutForm extends javax.swing.JDialog {
                             .add(layout.createSequentialGroup()
                                 .add(appTitleLabel)
                                 .add(44, 44, 44))
-                            .add(appDescLabel))
+                            .add(appDescLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE))
                         .add(16, 16, 16)
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                             .add(jLabel1)
