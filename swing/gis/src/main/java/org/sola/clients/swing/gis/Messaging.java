3ecdd07543dd64977befd06f85fe385c8904f2f9
Elton Manoku <elton.manoku@fao.org>
2012-05-13 10:36:01 +0000

    Fixed a problem with localisation [#95 state:fixed responsible:none]
    Resolving the bug that prevented getting the localized message.

--- swing/gis/src/main/java/org/sola/clients/swing/gis/Messaging.java
+++ swing/gis/src/main/java/org/sola/clients/swing/gis/Messaging.java
@@ -55,7 +55,7 @@ public class Messaging extends org.geotools.swing.extended.util.Messaging {
     @Override
     public void show(String messageCode, Object... args) {
         String msgSolaCode = this.getSolaMessageCode(messageCode);
-        MessageUtility.displayMessage(messageCode, args);
+        MessageUtility.displayMessage(msgSolaCode, args);
     }
 
     @Override
