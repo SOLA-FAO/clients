3ef5586cfeef32e933283e6570ef5ffa9d738598
Elton Manoku <elton.manoku@fao.org>
2012-08-13 16:58:46 +0200

    Fix add node tool [#201 state:fixed]

--- swing/gis/src/main/java/org/sola/clients/swing/gis/tool/CadastreRedefinitionAddNodeTool.java
+++ swing/gis/src/main/java/org/sola/clients/swing/gis/tool/CadastreRedefinitionAddNodeTool.java
@@ -1,28 +1,26 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO). All rights
+ * reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this list of conditions
+ * and the following disclaimer. 2. Redistributions in binary form must reproduce the above
+ * copyright notice,this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 /*
@@ -34,9 +32,11 @@ package org.sola.clients.swing.gis.tool;
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.linearref.LinearLocation;
 import com.vividsolutions.jts.linearref.LocationIndexedLine;
+import java.util.ArrayList;
 import java.util.List;
 import org.geotools.geometry.Envelope2D;
 import org.opengis.feature.simple.SimpleFeature;
+import org.sola.clients.swing.gis.beans.CadastreObjectBean;
 import org.sola.clients.swing.gis.beans.CadastreObjectNodeBean;
 import org.sola.clients.swing.gis.data.PojoDataAccess;
 import org.sola.clients.swing.gis.layer.CadastreRedefinitionNodeLayer;
@@ -81,9 +81,18 @@ public class CadastreRedefinitionAddNodeTool extends CadastreRedefinitionAbstrac
         if (nodeBean == null) {
             return;
         }
+        //nodeBean has already been added in the layer. Here it is retrieved as a feature
         SimpleFeature nodeFeature =
                 this.cadastreObjectNodeModifiedLayer.getFeatureByNodeId(nodeBean.getId());
-        this.insertNode(nodeFeature);
+
+        //The list of ids of cadastre objects that are related by this node is compiled here.
+        // Do not rely on the spatial check to retrieve the cadastre objects because they might
+        // be spatially different in the client than they are in the database.
+        List<String> cadastreObjectTargetIds = new ArrayList<String>();
+        for (CadastreObjectBean coBean : nodeBean.getCadastreObjectList()) {
+            cadastreObjectTargetIds.add(coBean.getId());
+        }
+        this.insertNode(nodeFeature, cadastreObjectTargetIds);
         if (!this.manipulateNode(nodeFeature)) {
             this.removeNode(nodeFeature);
         }
@@ -115,11 +124,15 @@ public class CadastreRedefinitionAddNodeTool extends CadastreRedefinitionAbstrac
      * @param nodeFeature
      * @param cadastreObjectTargetIds The target cadastre object ids
      */
-    private void insertNode(SimpleFeature nodeFeature) {
+    private void insertNode(SimpleFeature nodeFeature, List<String> cadastreObjectTargetIds) {
         Geometry nodeFeatureGeom = (Geometry) nodeFeature.getDefaultGeometry();
         Coordinate coordinate = nodeFeatureGeom.getCoordinate();
-        for(SimpleFeature cadastreObjectFeature: 
-                this.cadastreObjectModifiedLayer.getCadastreObjectFeatures(nodeFeature)){
+        for (String cadastreObjectId : cadastreObjectTargetIds) {
+            SimpleFeature cadastreObjectFeature =
+                    this.cadastreObjectModifiedLayer.getFeatureByCadastreObjectId(cadastreObjectId);
+            if (cadastreObjectFeature == null) {
+                continue;
+            }
             Polygon cadastreObjectGeom = (Polygon) cadastreObjectFeature.getDefaultGeometry();
             LinearRing exteriorRing = this.insertCoordinateInRing(
                     cadastreObjectGeom.getExteriorRing(), coordinate);
