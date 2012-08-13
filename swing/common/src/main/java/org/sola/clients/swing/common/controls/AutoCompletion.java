ca213d535e6dd1d6b07b3f78a5ec7ae5e9c690d2
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2012-08-13 11:18:56 +0200

    Dashboard automatically refreshed [#175 state:fixed responsible:none]

--- swing/common/src/main/java/org/sola/clients/swing/common/controls/AutoCompletion.java
+++ swing/common/src/main/java/org/sola/clients/swing/common/controls/AutoCompletion.java
@@ -1,18 +1,30 @@
 /*
- * Copyright 2012 Food and Agriculture Organization of the United Nations (FAO).
+  * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+*/
 package org.sola.clients.swing.common.controls;
 
 /**
@@ -27,10 +39,7 @@ import javax.swing.text.*;
 import org.sola.common.messaging.ClientMessage;
 import org.sola.common.messaging.MessageUtility;
 
-/* This work is hereby released into the Public Domain.
- * To view a copy of the public domain dedication, visit
- * http://creativecommons.org/licenses/publicdomain/
- */
+
 public class AutoCompletion extends PlainDocument {
     JComboBox comboBox;
     ComboBoxModel model;
@@ -149,19 +158,20 @@ public class AutoCompletion extends PlainDocument {
             offs = offs-str.length();
             // provide feedback to the user that his input has been received but can not be accepted
             comboBox.getToolkit().beep(); // when available use: UIManager.getLookAndFeel().provideErrorFeedback(comboBox);
-            MessageUtility.displayMessage(ClientMessage.CHECK_NO_MATCHING_ITEM,
-                            new Object[]{getText(0, getLength())});
-                    return;
+//            MessageUtility.displayMessage(ClientMessage.CHECK_NO_MATCHING_ITEM,
+//                            new Object[]{getText(0, getLength())});
+//                    return;
         }
        if (item != null)  {
         setText(item.toString());
         // select the completed part
         highlightCompletedText(offs+str.length());
-       } else {
-           MessageUtility.displayMessage(ClientMessage.CHECK_NO_MATCHING_ITEM,
-                            new Object[]{getText(0, getLength())});
-                    return;
-       }
+       } 
+//       else {
+//           MessageUtility.displayMessage(ClientMessage.CHECK_NO_MATCHING_ITEM,
+//                            new Object[]{getText(0, getLength())});
+//                    return;
+//       }
     }
     
     private void setText(String text) {
