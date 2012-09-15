b771a751b3686a4eefb60dde887e043ee0c1141a
Andrew McDowell <armcdowell@hotmail.com>
2012-09-15 15:35:28 +1200

    Update with candidate release version numbers

--- beans/src/main/java/org/sola/clients/beans/party/validation/PartyAddressValidator.java
+++ beans/src/main/java/org/sola/clients/beans/party/validation/PartyAddressValidator.java
@@ -1,4 +1,8 @@
-/*
+/**
+ * ******************************************************************************************
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
