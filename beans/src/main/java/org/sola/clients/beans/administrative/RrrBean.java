dc1f58f0fee98bf4dd6a0294125b79997d47ffe9
amcdowell <armcdowell@hotmail.com>
2013-03-06 10:56:44 +1300

    Fixed so party is unlinked from RRR when share is removed.
    Fixed in v2.0.
    [#256 state:fixed responsible:none milestone:v2.0]

--- beans/src/main/java/org/sola/clients/beans/administrative/RrrBean.java
+++ beans/src/main/java/org/sola/clients/beans/administrative/RrrBean.java
@@ -1,28 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.clients.beans.administrative;
@@ -30,6 +32,7 @@ package org.sola.clients.beans.administrative;
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.ListIterator;
 import javax.validation.Valid;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
@@ -54,9 +57,9 @@ import org.sola.common.messaging.ClientMessage;
 import org.sola.webservices.transferobjects.EntityAction;
 import org.sola.webservices.transferobjects.administrative.RrrTO;
 
-/** 
- * Contains properties and methods to manage <b>RRR</b> object of the 
- * domain model. Could be populated from the {@link RrrTO} object.
+/**
+ * Contains properties and methods to manage <b>RRR</b> object of the domain
+ * model. Could be populated from the {@link RrrTO} object.
  */
 public class RrrBean extends AbstractTransactionedBean {
 
@@ -68,7 +71,6 @@ public class RrrBean extends AbstractTransactionedBean {
     public static final String CODE_APARTMENT = "apartment";
     public static final String CODE_STATE_OWNERSHIP = "stateOwnership";
     public static final String CODE_MORTGAGE = "mortgage";
-    
     public static final String CODE_AGRI_ACTIVITY = "agriActivity";
     public static final String CODE_COMMON_OWNERSHIP = "commonOwnership";
     public static final String CODE_CUSTOMARY_TYPE = "customaryType";
@@ -86,7 +88,6 @@ public class RrrBean extends AbstractTransactionedBean {
     public static final String CODE_MONUMENT = "monument";
     public static final String CODE_LIFE_ESTATE = "lifeEstate";
     public static final String CODE_CAVEAT = "caveat";
-    
     public static final String BA_UNIT_ID_PROPERTY = "baUnitId";
     public static final String TYPE_CODE_PROPERTY = "typeCode";
     public static final String RRR_TYPE_PROPERTY = "rrrType";
@@ -103,7 +104,6 @@ public class RrrBean extends AbstractTransactionedBean {
     public static final String SELECTED_SHARE_PROPERTY = "selectedShare";
     public static final String SELECTED_PROPERTY = "selected";
     public static final String SELECTED_RIGHTHOLDER_PROPERTY = "selectedRightHolder";
-    
     private String baUnitId;
     private String nr;
     @Past(message = ClientMessage.CHECK_REGISTRATION_DATE, payload = Localized.class)
@@ -111,7 +111,7 @@ public class RrrBean extends AbstractTransactionedBean {
     private String transactionId;
     @NotNull(message = ClientMessage.CHECK_NOTNULL_EXPIRATION, payload = Localized.class, groups = {MortgageValidationGroup.class})
     @Future(message = ClientMessage.CHECK_FUTURE_EXPIRATION, payload = Localized.class,
-    groups = {MortgageValidationGroup.class})
+            groups = {MortgageValidationGroup.class})
     private Date expirationDate;
     @NotNull(message = ClientMessage.CHECK_NOTNULL_MORTGAGEAMOUNT, payload = Localized.class, groups = {MortgageValidationGroup.class})
     private BigDecimal mortgageAmount;
@@ -350,7 +350,14 @@ public class RrrBean extends AbstractTransactionedBean {
     }
 
     public void removeSelectedRrrShare() {
+        // Issue #256 Unlink Party from RRR when removing share. 
         if (selectedShare != null && rrrShareList != null) {
+            if (getRightHolderList().size() > 0) {
+                ListIterator<PartySummaryBean> it = getRightHolderList().listIterator();
+                while (it.hasNext()) {
+                    getRightHolderList().safeRemove(it.next(), EntityAction.DISASSOCIATE);
+                }
+            }
             rrrShareList.safeRemove(selectedShare, EntityAction.DELETE);
         }
     }
@@ -377,7 +384,7 @@ public class RrrBean extends AbstractTransactionedBean {
     private ObservableList<PartySummaryBean> getFilteredOwnersList() {
         return rightHolderList.getFilteredList();
     }
-    
+
     public void setRightHolderList(SolaList<PartySummaryBean> rightHolderList) {
         this.rightHolderList = rightHolderList;
     }
@@ -402,12 +409,12 @@ public class RrrBean extends AbstractTransactionedBean {
         propertySupport.firePropertyChange(SELECTED_PROPERTY, oldValue, this.selected);
     }
 
-    public void removeSelectedRightHolder(){
-        if(selectedRightholder!=null){
+    public void removeSelectedRightHolder() {
+        if (selectedRightholder != null) {
             getRightHolderList().safeRemove(selectedRightholder, EntityAction.DISASSOCIATE);
         }
     }
-    
+
     public void addOrUpdateRightholder(PartySummaryBean rightholder) {
         if (rightholder != null && rightHolderList != null) {
             if (rightHolderList.contains(rightholder)) {
@@ -417,7 +424,7 @@ public class RrrBean extends AbstractTransactionedBean {
             }
         }
     }
-    
+
     public RrrBean makeCopyByAction(RRR_ACTION rrrAction) {
         RrrBean copy = this;
 
@@ -439,9 +446,11 @@ public class RrrBean extends AbstractTransactionedBean {
         return copy;
     }
 
-    /** 
-     * Generates new ID, RowVerion and RowID. 
-     * @param resetChildren If true, will change ID fields also for child objects.
+    /**
+     * Generates new ID, RowVerion and RowID.
+     *
+     * @param resetChildren If true, will change ID fields also for child
+     * objects.
      * @param removeBaUnitId If true, will set <code>BaUnitId</code> to null.
      */
     public void resetIdAndVerion(boolean resetChildren, boolean removeBaUnitId) {
