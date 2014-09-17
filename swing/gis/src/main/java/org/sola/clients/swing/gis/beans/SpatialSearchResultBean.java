c3365f2b90cdeb33880385c85504db1b0478aa84
amcdowell <armcdowell@hotmail.com>
2014-09-17 13:29:35 +1200

    Modifications to approval for State Land Change tasks. Improvements to Map Searches

--- swing/gis/src/main/java/org/sola/clients/swing/gis/beans/SpatialSearchResultBean.java
+++ swing/gis/src/main/java/org/sola/clients/swing/gis/beans/SpatialSearchResultBean.java
@@ -1,28 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2014 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2014 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.clients.swing.gis.beans;
@@ -30,8 +32,8 @@ package org.sola.clients.swing.gis.beans;
 import org.sola.clients.beans.AbstractBindingBean;
 
 /**
- * Bean used for the result in the Spatial Object Search Result list. Provides a customized toString
- * method for display in the list control.
+ * Bean used for the result in the Spatial Object Search Result list. Provides a
+ * customized toString method for display in the list control.
  *
  * @author soladev
  */
@@ -40,9 +42,10 @@ public class SpatialSearchResultBean extends AbstractBindingBean implements Comp
     private String id;
     private String label;
     private byte[] theGeom;
+    private int sortIdx;
 
     /**
-     * Constructor for the bean. 
+     * Constructor for the bean.
      */
     public SpatialSearchResultBean() {
         super();
@@ -72,16 +75,27 @@ public class SpatialSearchResultBean extends AbstractBindingBean implements Comp
         this.theGeom = theGeom; //NOSONAR
     }
 
+    public int getSortIdx() {
+        return sortIdx;
+    }
+
+    public void setSortIdx(int sortIdx) {
+        this.sortIdx = sortIdx;
+    }
+
     @Override
     public String toString() {
         return getLabel();
     }
 
     /**
-     * Can be used to sort the Spatial Result List 
+     * Can be used to sort the Spatial Result List
      */
     @Override
     public int compareTo(Object compareObj) {
-        return this.toString().compareTo(compareObj.toString());
+        int compareSortIdx = ((SpatialSearchResultBean) compareObj).getSortIdx(); 
+        String sortKey = String.format("%04d", sortIdx) + this.toString();
+        String compareKey = String.format("%04d", compareSortIdx) + compareObj.toString();
+        return sortKey.compareTo(compareKey);
     }
 }
