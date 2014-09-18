a11d375d922574b0d127c9fed9e33351a10195e8
amcdowell <armcdowell@hotmail.com>
2014-09-18 14:35:37 +1200

    Version Number v012 update

--- swing/ui/src/main/java/org/sola/clients/swing/ui/localization/LocalizationManager.java
+++ swing/ui/src/main/java/org/sola/clients/swing/ui/localization/LocalizationManager.java
@@ -49,7 +49,7 @@ public class LocalizationManager {
     private static final String PRODUCTION_HOST_IP = "46.149.21.37";
     //private static final String PRODUCTION_HOST_NAME = "localhost";
     //private static final String PRODUCTION_HOST_IP = "127.0.0.1";
-    private static final String SOLA_VERSION = "v011";
+    private static final String SOLA_VERSION = "v012";
 
     /**
      * Loads default language and country codes and sets {@link Locale} settings
